b060cee9a698a0fd7549e036c0edbb2abd389ae5
Sungkuk Kim <sungkuk5420@gmail.com>
2023-10-08 18:12:07 +0900

    todo list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-
-import { useState,useEffect } from "react";
-
-function Hello(){
-  useEffect(()=>{
-    console.log("created")
-    return ()=>console.log("destroyed")
-  },[])
-  return <h1>Hello</h1>
-}
+import { useState } from "react";
 
 function App() {
-  const [showing,setShowing] = useState(false);
-  const onClick= () => setShowing((prev) => !prev);
+  const [todo,setTodo] = useState("");
+  const [todos,setTodos] = useState([]);
+  const onChange = (event) => {setTodo(event.target.value);
+    console.log(todo)
+  }
+  const onSubmit = (event)=>{event.preventDefault();
+    if(todo === ""){
+      return;
+    }
+    console.log(todo)
+    setTodos(currentArray => [todo,...currentArray])
+    setTodo('');
+  }
   return (
-    <div className="App">
-      {showing?<Hello/>:null}
-      <button  onClick={onClick}>{showing?'hide':'show'}</button>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} type="text" value={todo} placeholder="write your to do" />
+        <button>Add To Do</button>
+      </form>
+      <ul>
+        {todos.map((item,index)=>{          
+          return <li key={index} >{item}</li>
+        })}
+      </ul>
     </div>
   );
 }
